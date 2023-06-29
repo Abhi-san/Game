c97613cdd782372cdd70076f9f9feb6bad7b1054
Abhi-san <76156862+Abhi-san@users.noreply.github.com>
2023-06-29 18:41:45 +0530

    Update manage.js

--- manage.js
+++ manage.js
@@ -15,71 +15,11 @@ app.use(methodOverride(function (req, res) {
 }));
 
 let todolist = [];
-let playGame = true;
-let guess = 0; // Default guess value
-
-app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-// Generate a random number between 1 and 100
-function getRandomNumber() {
-  return Math.floor(Math.random() * 100) + 1;
-}
-
-// Check if the guess is correct
-function checkGuess(randomNumber, guess) {
-  if (guess === randomNumber) {
-    return 'You guessed correctly!';
-  } else if (guess < randomNumber) {
-    return 'Too low! Try again!';
-  } else if (guess > randomNumber) {
-    return 'Too high! Try again!';
-  }
-}
-
-// Validate the user's guess
-function validateGuess(randomNumber, guess) {
-  if (isNaN(guess)) {
-    return 'Please enter a valid number';
-  } else if (guess < 1) {
-    return 'Please enter a number greater than 1!';
-  } else if (guess > 100) {
-    return 'Please enter a number less than 100!';
-  } else {
-    return checkGuess(randomNumber, guess);
-  }
-}
-
-// Play the game
-function playGameFunc(randomNumber, guess) {
-  if (!playGame) {
-    return 'The game has ended. Start a new game.';
-  }
-
-  const validationResult = validateGuess(randomNumber, guess);
-  playGame = validationResult !== 'You guessed correctly!';
-
-  return validationResult;
-}
-
-app.get('/todo/:id', function (req, res) {
-  let todoIdx = req.params.id;
-  let todo = todolist[todoIdx];
-
-  if (todo) {
-    const randomNumber = getRandomNumber();
-    res.render('edititem.ejs', {
-      todoIdx,
-      todo,
-      clickHandler: "func1();",
-      playGame,
-      guess,
-      play: playGameFunc.bind(null, randomNumber)
-    });
-  } else {
-    res.redirect('/todo');
-  }
+app.get('/todo', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/todo/add/', function (req, res) {
